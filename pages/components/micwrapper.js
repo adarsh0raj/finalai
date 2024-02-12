ddb7ea7cbfa92216b71ec10b47a1e4e0483fe755
adarsh <rajadarsh2602@gmail.com>
2024-02-12 22:50:00 +0530

    initial + camera setup

---
+++ pages/components/micwrapper.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const MicWrapper = () => {
+    return (
+        <h1>
+            lorem ipsum dolor sit amet  consectetur adipisicing elit.
+        </h1>
+    );
+}
+
+export default MicWrapper;
